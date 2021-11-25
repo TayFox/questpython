56ec29dc4ab3819ceec9dfdcfc20cae3d43b8401
TayFox <wolf2335463@gmail.com>
2021-11-25 23:17:56 +0300

    Оптимизация кода, убирание лишнего мусора

--- quest.py
+++ quest.py
@@ -1,41 +1,22 @@
+
 x = input("Введите число: ")
 flag = False
 
-if x.isnumeric() == False:		# Защита от дроби
-    print('NO')
-elif int(x) <= 9:			# Защита от отрицательных чисел и 0
+if x.isnumeric() and int(x) <= 9:                   # Фильтр вводимых данных
     print('NO')
 else:
-    flag = True				# Присвоение значений
+    flag = True
     xwork = int(x)
     x11 = int(len(x)*"1")
-#    print('Полученное число: ',xwork)
-#    print('Первый x11: ', x11)
 
 while flag == True:
-    if xwork > 0:
-#        print('Новый цикл')
-#        print(xwork,"-",x11)
+    if (xwork-x11) > 0 and len(str(x11)) > 1:       # Проверка неотрицательного ответа и соответствия x11
         xwork = xwork - x11
-#        print(xwork)
-        if xwork < 0:			# Коррекция
-#            print('Коррекция')
-            xwork = xwork + x11
-            x11 = str(x11)
-            x11 = x11[:-1]
-            if len(x11) <= 1:
-#                print("x11 < 1 !!!")
-                print('NO')
-                break
-            x11 = int(x11)
-#            print(x11)
-        if xwork % 11 == 0:		# Проверка деления на 11
-            print('YES')
-            break
-        else:
-            continue
-    elif xwork == 0:
+    elif (xwork-x11) < 0:                           # Коррекция x11
+        x11 = int(str(x11)[:-1])
+    elif xwork == 0 or xwork % 11 == 0:             # Проверка возможности правильного ответа
         print('YES')
         break
     else:
-        print('NO')
+        print('NO')                                 # Вывод отрицательного ответа
+
