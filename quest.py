b3b409f97ad58fef24699f433cf4b62631de8b7f
TayFox <wolf2335463@gmail.com>
2021-11-25 23:46:43 +0300

    Восстановление работоспособности и добавление функции

--- quest.py
+++ quest.py
@@ -1,22 +1,19 @@
-
-x = input("Введите число: ")
-flag = False
-
-if x.isnumeric() and int(x) <= 9:                   # Фильтр вводимых данных
-    print('NO')
-else:
-    flag = True
-    xwork = int(x)
-    x11 = int(len(x)*"1")
-
-while flag == True:
-    if (xwork-x11) > 0 and len(str(x11)) > 1:       # Проверка неотрицательного ответа и соответствия x11
-        xwork = xwork - x11
-    elif (xwork-x11) < 0:                           # Коррекция x11
-        x11 = int(str(x11)[:-1])
-    elif xwork == 0 or xwork % 11 == 0:             # Проверка возможности правильного ответа
-        print('YES')
-        break
+def x11(x):
+    if x.isnumeric() and int(x) <= 9:                   # Фильтр вводимых данных
+        return 'NO'
+        flag = False
     else:
-        print('NO')                                 # Вывод отрицательного ответа
+        flag = True
+        xwork = int(x)
+        x11 = int(len(x)*"1")
 
+    while flag:
+        if (xwork-x11) > 0 and len(str(x11)) > 1:       # Проверка неотрицательного ответа и соответствия x11
+            xwork = xwork - x11
+        elif (xwork-x11) < 0:                           # Коррекция x11
+            x11 = int(str(x11)[:-1])
+        else:
+            return 'NO'
+        if xwork == 0 or xwork % 11 == 0:  # Проверка возможности правильного ответа
+            return 'YES'
+print(x11(input('Введите число: ')))
