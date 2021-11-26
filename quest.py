5b3d457b865d57552d96d45ebb2cc52de1288380
TayFox <wolf2335463@gmail.com>
2021-11-26 17:10:33 +0300

    Добавлена функция тестирования диапазона чисел. Добавлена обработка ошибок.

--- quest.py
+++ quest.py
@@ -1,19 +1,40 @@
 def x11(x):
-    if x.isnumeric() and int(x) <= 9:                   # Фильтр вводимых данных
-        return 'NO'
-        flag = False
+    try:
+        int(x)
+    except ValueError:
+        return 'This is string!'
+    if not x.isnumeric() or int(x) <= 10:					# Фильтр вводимых данных
+        return 'Invalid number or string'
     else:
-        flag = True
         xwork = int(x)
         x11 = int(len(x)*"1")
 
-    while flag:
-        if (xwork-x11) > 0 and len(str(x11)) > 1:       # Проверка неотрицательного ответа и соответствия x11
+    while True:
+        if (xwork-x11) > 0 and len(str(x11)) > 1 and xwork % 11 != 0:		# Проверка неотрицательного ответа и соответствия x11
             xwork = xwork - x11
-        elif (xwork-x11) < 0:                           # Коррекция x11
+        elif (xwork-x11) < 0:                                               	# Коррекция x11
             x11 = int(str(x11)[:-1])
+        elif xwork == 0 or xwork % 11 == 0 or (xwork-x11) == 0:             	# Проверка возможности правильного ответа
+            return 'YES'
         else:
             return 'NO'
-        if xwork == 0 or xwork % 11 == 0:  # Проверка возможности правильного ответа
-            return 'YES'
-print(x11(input('Введите число: ')))
+
+def test1m(x):                      						# Проверка чисел до 1000000
+    answers = [0, 0, 0]
+    print('Программа запущена...')
+    for i in range(int(x)):
+            if x11(i) == 'YES':
+                answers[0] += 1
+            elif x11(i) == 'NO':
+                answers[1] += 1
+            else:
+                answers[2] +=1
+    print('yes: ', answers[0])
+    print('no: ', answers[1])
+    print('worng: ', answers[2])
+    return ''
+
+#print('Введите конец диапазона тестирования (0-...): ',end='')
+#print(test1m(input()))
+
+print(x11(input()))
