bdbcf47aac7f05a9cab78e699bf849d60509ba43
TayFox <wolf2335463@gmail.com>
2021-12-10 20:57:32 +0300

    finally

--- quest.py
+++ quest.py
@@ -1,40 +1,56 @@
-def x11(x):
+# IntInputOnly() возвращает число или False, если введена строка или неправильная дробь
+# x11() основной рабочий цикл. Принимает количество итераций и тестируемые числа
+
+def IntInputOnly(data):
+    if data == '':
+        print('Пустой ввод.')
+        return False
     try:
-        int(x)
+        data = float(data)
     except ValueError:
-        return 'This is string!'
-    if not x.isnumeric() or int(x) <= 10:					# Фильтр вводимых данных
-        return 'Invalid number or string'
+        print('Это строка!')
+        return False
+    if (data % 1) != 0:
+        print('Неправильная дробь!')
+        return False
+    elif data < 0:
+        print('Меньше нуля!')
+        return False
     else:
-        xwork = int(x)
-        x11 = int(len(x)*"1")
+        return int(data)
 
-    while True:
-        if (xwork-x11) > 0 and len(str(x11)) > 1 and xwork % 11 != 0:		# Проверка неотрицательного ответа и соответствия x11
-            xwork = xwork - x11
-        elif (xwork-x11) < 0:                                               	# Коррекция x11
-            x11 = int(str(x11)[:-1])
-        elif xwork == 0 or xwork % 11 == 0 or (xwork-x11) == 0:             	# Проверка возможности правильного ответа
-            return 'YES'
+
+def x11(t):
+    for i in range(IntInputOnly(t)):
+        x = IntInputOnly(input('Введите число #' + str(i + 1) + ': '))
+        # Фильтр вводимых данных
+        if int(x) > 11:
+            xwork = int(x)
+            x11 = int(len(str(x)) * "1")
+        elif not x:
+            continue
         else:
-            return 'NO'
-
-def test1m(x):                      						# Проверка чисел до 1000000
-    answers = [0, 0, 0]
-    print('Программа запущена...')
-    for i in range(int(x)):
-            if x11(i) == 'YES':
-                answers[0] += 1
-            elif x11(i) == 'NO':
-                answers[1] += 1
+            print('Слишком маленькое число (< 11)')
+            continue
+        while True:
+            # Проверка неотрицательного ответа и соответствия x11
+            if (xwork - x11) >= 0 and x11 >= 11 and xwork % 11 != 0:
+                # print('Вычитание ', xwork, '-', x11)
+                xwork = xwork - x11
+            # Коррекция x11
+            elif (xwork - x11) < 0 and not x11 < 11:
+                # print('Коррекция')
+                x11 = int(str(x11)[:-1])
+            # Проверка возможности правильного ответа
+            elif xwork == 0 or xwork % 11 == 0:
+                # print('Подтверждение')
+                print('YES')
+                break
             else:
-                answers[2] +=1
-    print('yes: ', answers[0])
-    print('no: ', answers[1])
-    print('worng: ', answers[2])
+                # print('Отрицание')
+                print('NO')
+                break
     return ''
 
-#print('Введите конец диапазона тестирования (0-...): ',end='')
-#print(test1m(input()))
 
-print(x11(input()))
+print(x11(input('Введите количество параметров: ')))
